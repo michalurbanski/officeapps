144b71a5f244e88c3f1f8f892f2b822d38c021da
michalurbanski <michal_urbanski@windowslive.com>
2015-05-24 19:54:22 +0000

    Adds sample body content handling and option to add yourself to bcc users

--- FirstAppForOutlook/FirstAppForOutlookWeb/AppCompose/Home/Home.js
+++ FirstAppForOutlook/FirstAppForOutlookWeb/AppCompose/Home/Home.js
@@ -9,13 +9,38 @@
             app.initialize();
 
             $('#set-subject').click(setSubject);
+            $('#set-body').click(setBody); 
             $('#get-subject').click(getSubject);
             $('#add-to-recipients').click(addToRecipients);
+            $('#add-to-bcc').click(addToBcc);
         });
     };
 
     function setSubject() {
         Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync("Hello world!");
+    }
+
+    function setBody() {
+        // Create html body 
+        var mainDiv = $("<div>");
+        mainDiv.append($("<h2>").text("This is predefined body - fill with your own content"));
+
+        var item = Office.cast.item.toItemCompose(Office.context.mailbox.item);
+
+        // We need to convert created content to html here (note usage of coercion type)
+        item.body.setSelectedDataAsync(mainDiv.html(), {coercionType: Office.CoercionType.Html});
+    }
+
+    function addToBcc() {
+        var item = Office.context.mailbox.item;
+        var addressToAdd = {
+            displayName: Office.context.mailbox.userProfile.displayName,
+            emailAddress: Office.context.mailbox.userProfile.emailAddress
+        };
+
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            Office.cast.item.toMessageCompose(item).bcc.addAsync([addressToAdd]);
+        }
     }
 
     function getSubject() {
