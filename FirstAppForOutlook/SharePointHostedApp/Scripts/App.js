90acfb60ecc0c1c71045f13b41fc55a315c74069
michalurbanski <michal_urbanski@windowslive.com>
2015-06-05 17:38:11 +0000

    Gets user name using REST

--- FirstAppForOutlook/SharePointHostedApp/Scripts/App.js
+++ FirstAppForOutlook/SharePointHostedApp/Scripts/App.js
@@ -2,10 +2,10 @@
     "use strict";
 
     $(function () {
-        getUserNameUsingJsom(); 
+        //getUserNameUsingJsom(); 
+        getUserNameUsingRest(); 
     });
 }());
-
 
 function getUserNameUsingJsom() {
     var context = SP.ClientContext.get_current();
@@ -20,4 +20,20 @@ function getUserNameUsingJsom() {
             $('#message').text("Error getting user username. Error: " + args.get_message())
         }));
 
+};
+
+function getUserNameUsingRest() {
+    $.ajax({
+        url: "../_api/web/currentuser",
+        type: "GET",
+        headers: {
+            "accept": "application/json;odata=verbose", //NOTE: headers are mandatory!
+        },
+        success: function(data, status, jqXHR){
+            $('#message').text("Hello " + data.d.Title);
+        },
+        error: function(jqXHR, status, message){
+            $('#message').text("Error getting user username. Error: " + message);
+        }
+    });
 };
