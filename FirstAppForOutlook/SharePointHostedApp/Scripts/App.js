1addffe273d18389c8be34867e10976b5c486af6
michalurbanski <michal_urbanski@windowslive.com>
2015-06-05 17:30:23 +0000

    Refactors app.js for jsom call

--- FirstAppForOutlook/SharePointHostedApp/Scripts/App.js
+++ FirstAppForOutlook/SharePointHostedApp/Scripts/App.js
@@ -2,16 +2,22 @@
     "use strict";
 
     $(function () {
-        var context = SP.ClientContext.get_current();
-        var user = context.get_web().get_currentUser();
-
-        context.load(user);
-        context.executeQueryAsync(
-            Function.createDelegate(this, function () {
-                $('#message').text("Hello " + user.get_title())
-            }),
-            Function.createDelegate(this, function (args) {
-                $('#message').text("Error getting user username. Error: " + args.get_message())
-            }));
+        getUserNameUsingJsom(); 
     });
 }());
+
+
+function getUserNameUsingJsom() {
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    context.load(user);
+    context.executeQueryAsync(
+        Function.createDelegate(this, function () {
+            $('#message').text("Hello " + user.get_title())
+        }),
+        Function.createDelegate(this, function (args) {
+            $('#message').text("Error getting user username. Error: " + args.get_message())
+        }));
+
+};
